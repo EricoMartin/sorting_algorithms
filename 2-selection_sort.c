5a7e187355c7c159a7ff50bece881e1830f02a4e
EricoMartin <ericomartin.0785@gmail.com>
2023-10-29 13:47:10 -0700

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * selection_sort - uses the selection algo to sort
+ * an array
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min_val, min_index, flag;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min_val = array[i];
+		min_index = i;
+		flag = 0;
+
+		for (j = i + 1; j < size; j++)	/* start from the unsorted part */
+		{
+			if (min_val > array[j])  /* check for min value */
+			{
+				min_val = array[j];
+				min_index = j;
+				flag = 1;
+			}
+		}
+
+		if (flag)
+		{
+			holder = array[i];
+			array[i] = array[min_index];
+			array[min_index] = holder;
+			print_array(array, size);
+		}
+	}
+}
