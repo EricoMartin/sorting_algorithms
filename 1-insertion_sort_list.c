eeac4b419a978292cf178ccb5efdd25b5c3119f0
EricoMartin <ericomartin.0785@gmail.com>
2023-10-29 13:20:52 -0700

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - A function that sorts a doubly
+ * linked list of integers in ascending order
+ *
+ * @list: A doubly linked list
+ *
+ * Returns: Nothing
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *temp;
+
+
+	if (!list)
+		return;
+
+	for (curr = *list; curr; curr = curr->next)
+	{
+		while (curr->next && (curr->next->n < curr->n))
+		{
+			temp = curr->next;
+			curr->next = temp->next
+			temp->prev = curr->prev
+		}
+
+		if (curr->prev)
+			curr->prev->next = temp;
+
+		if (temp->next)
+			temp->next->prev = curr;
+
+		curr->prev = temp;
+		temp->curr = prev;
+
+		if (temp->prev)
+			curr = temp->prev;
+		else
+			*list = temp;
+
+		print_list(*list);
+	}
+}
